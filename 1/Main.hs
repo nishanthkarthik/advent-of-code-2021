b4917cadbda34030bd6afcd7bbf2d3040edfc775
Karthik Nishanth <nishanthkarthik@live.com>
2021-12-01 12:23:48 +0100

    Simplify day 1

--- 1/Main.hs
+++ 1/Main.hs
@@ -2,11 +2,8 @@ import qualified Data.List as List
 
 testData = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 
-numberOfIncreases :: (Num a, Ord a) => [a] -> Int
-numberOfIncreases x@(_:xs) = length $ filter (> 0) $ zipWith (-) xs x
-
-movingWindow :: Int -> [a] -> [[a]]
-movingWindow n = foldr (zipWith (:)) (repeat []) . take n . List.tails
+numberOfIncreases :: (Num a, Ord a) => [a] -> [a] -> Int
+numberOfIncreases l r = length $ filter id $ zipWith (>) r l
 
 readInput :: IO [Integer]
 readInput = do
@@ -15,5 +12,5 @@ readInput = do
 
 main = do
     i <- readInput
-    print $ numberOfIncreases i
-    print $ numberOfIncreases $ map sum $ movingWindow 3 i
+    print $ numberOfIncreases i $ tail i
+    print $ numberOfIncreases i $ drop 3 i
