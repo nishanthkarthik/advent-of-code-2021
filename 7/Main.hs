04aa0fa050f00ff1b84054f0ed3f9903d21b6102
Karthik Nishanth <nishanthkarthik@live.com>
2021-12-07 22:09:36 +0100

    optimize day 7

--- 7/Main.hs
+++ 7/Main.hs
@@ -5,16 +5,17 @@ import Data.Void (Void)
 import Data.Either (fromRight)
 import qualified Text.Megaparsec.Char.Lexer as Lex
 import Control.Monad (void)
+import Data.List (sort, scanl1, scanr1)
 
 type Parser = Parsec Void Text
 
-parser :: Parser [Integer]
+parser :: Parser [Int]
 parser = many $ do
     void $ optional (char ',')
     a <- Lex.decimal
     return a
 
-readInput :: String -> IO [Integer]
+readInput :: String -> IO [Int]
 readInput f = do
     text <- readFile f
     return (fromRight [] $ parse parser "" $ pack text)
@@ -22,7 +23,14 @@ readInput f = do
 main :: IO ()
 main = do
     i <- readInput "7/input.txt"
-    let ap n = (n * (n + 1)) `div` 2
-        stoplimit = 2 * (foldl1 max i)
-    print $ foldl1 min [sum $ map (abs . subtract it) i | it <- i]
-    print $ foldl1 min [sum $ map (ap . abs . subtract it) i | it <- [0..stoplimit]]
+    let d = sort i
+        presum = scanl1 (+) d
+        postsum = scanr1 (+) d ++ [0]
+        expr1 = (\(idx,it) -> (2 * idx + 2 - length d) * it + (postsum !! (idx + 1)) - (presum !! idx))
+    print $ foldl1 min $ map expr1 $ zip [0..] d
+
+    let total = fromIntegral (sum d) :: Double
+        n = fromIntegral (length d) :: Double
+        minimaj = (floor $ 0.5 + (total / n)) - 1
+        ap n = (n * (n + 1)) `div` 2
+    print $ foldl1 min [sum $ map (ap . abs . (subtract it)) d | it <- [minimaj-2..minimaj+2] ]
