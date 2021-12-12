b2ad78012cc9d18eb12e3dd30c5f055bcef736cb
Karthik Nishanth <nishanthkarthik@live.com>
2021-12-12 20:08:17 +0100

    day 12: switch to Text

--- 12/Main.hs
+++ 12/Main.hs
@@ -1,7 +1,7 @@
 import Text.Megaparsec
 import Text.Megaparsec.Char (letterChar, char, newline)
 import Data.Void (Void)
-import Data.Text (Text, pack)
+import Data.Text (Text, pack, all)
 import Control.Monad (void)
 import Data.Either (fromRight)
 import Data.Maybe
@@ -14,37 +14,37 @@ import qualified Data.Map.Strict as Map
 
 type Parser = Parsec Void Text
 
-parser :: Parser [(String, String)]
+parser :: Parser [(Text, Text)]
 parser = many $ do
     from <- some letterChar
     void $ char '-'
     to <- some letterChar
     void $ optional newline
-    return (from, to)
+    return (pack from, pack to)
 
-type Visitor = [String] -> [String] -> [String]
-type Graph = Map.Map String [String]
+type Visitor = [Text] -> [Text] -> [Text]
+type Graph = Map.Map Text [Text]
 
 visitor1 :: Visitor
 visitor1 visited neighbors = manyTimes ++ (oneTime \\ visited)
-    where (manyTimes, oneTime) = partition (all isUpper) neighbors
+    where (manyTimes, oneTime) = partition (Data.Text.all isUpper) neighbors
 
 visitor2 :: Visitor
 visitor2 visited neighbors = upper ++ lowerVisits
-    where (upper, lower) = partition (all isUpper) (neighbors \\ ["start"])
+    where (upper, lower) = partition (Data.Text.all isUpper) (neighbors \\ [pack "start"])
           countElem arr x = (length . filter (== x)) arr
-          visitedLower = filter (all isLower) visited
+          visitedLower = filter (Data.Text.all isLower) visited
           alreadyVisitedALowerTwice = (length visitedLower) /= (length $ nubOrd visitedLower)
           lowerVisits = if alreadyVisitedALowerTwice then (lower \\ visited) else lower
 
 -- Adj list -> Visitor -> Visited -> Cur -> PathCount
-bfs :: Graph -> Visitor -> [String] -> String -> Int
+bfs :: Graph -> Visitor -> [Text] -> Text -> Int
 bfs graph visitor visited cur
-    | cur == "end" = 1
+    | cur == (pack "end") = 1
     | otherwise = sum $ map (bfs graph visitor (cur : visited)) visits
     where visits = visitor (cur : visited) (graph Map.! cur)
 
-readInput :: String -> IO [(String, String)]
+readInput :: String -> IO [(Text, Text)]
 readInput f = do
     text <- readFile f
     return (fromRight [] $ parse parser "" $ pack text)
@@ -53,11 +53,11 @@ main :: IO ()
 main = do
     i <- readInput "12/input.txt"
     let graph = makeGraph i
-        makeGraph :: [(String, String)] -> Graph
+        makeGraph :: [(Text, Text)] -> Graph
         makeGraph xs = Map.fromList keyvals
-            where undirected xs = filter (\(a, b) -> a /= "end" && b /= "start") (xs ++ map swap xs)
-                  pairs = groupBy ((==) `on` fst) $ sort $ undirected xs
-                  keyvals = map (\it -> ((fst . head) it, map snd it)) pairs
+        undirected xs = filter (\(a, b) -> a /= (pack "end") && b /= (pack "start")) (xs ++ map swap xs)
+        pairs = groupBy ((==) `on` fst) $ sort $ undirected i
+        keyvals = map (\it -> ((fst . head) it, map snd it)) pairs
 
-    print $ bfs graph visitor1 [] "start"
-    print $ bfs graph visitor2 [] "start"
+    print $ bfs graph visitor1 [] $ pack "start"
+    print $ bfs graph visitor2 [] $ pack "start"
