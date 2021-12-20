4dec1383992f3c0909b4775e4da73efe562c2ce9
Karthik Nishanth <nishanthkarthik@live.com>
2021-12-20 21:37:33 +0100

    update day 20

--- 20/Main.hs
+++ 20/Main.hs
@@ -1,6 +1,5 @@
 import Data.Array (listArray, Array, (!))
 import qualified Data.Set as Set
-import Data.Function (on)
 
 type Algo = Array Int Int
 type Image = Set.Set (Int, Int)
@@ -9,45 +8,34 @@ readInput :: IO (Algo, Image)
 readInput = do
     text <- lines <$> readFile "20/input.txt"
     let algoText = head text
-        bitOf c = if c == '#' then True else False
-        algo = listArray (0, length algoText - 1) $ map (fromEnum . bitOf) algoText
+        algo = listArray (0, length algoText - 1) $ map (fromEnum . (== '#')) algoText
     let imageText = drop 2 text
         lx = length imageText
         ly = length $ head imageText
         imageList = zip [(i, j) | i <- [0..lx-1], j <- [0..ly-1]] (concat imageText)
-        image = (Set.fromList . map fst . filter (bitOf . snd)) imageList
+        image = (Set.fromList . map fst . filter ((== '#') . snd)) imageList
     return (algo, image)
 
-kernelWindow :: (Int, Int) -> [(Int, Int)]
-kernelWindow (i, j) = [(i + di, j + dj) | di <- [-1..1], dj <- [-1..1]]
-
-showImage :: Image -> String
-showImage img = unlines grid
-    where ((minX, minY), (maxX, maxY)) = boundsImage img
-          pixel i = if i == 1 then '#' else '.'
-          grid = [[(pixel . safeLookup img) (x, y) | y <- [minY..maxX]] | x <- [minX..maxX]]
-
-boundsImage :: Image -> ((Int, Int), (Int, Int))
-boundsImage img = ((minX, minY), (maxX, maxY))
+bounds :: Image -> ((Int, Int), (Int, Int))
+bounds img = ((minX, minY), (maxX, maxY))
     where minX = fst $ Set.findMin img
           maxX = fst $ Set.findMax img
           folder fn ac it = fn ac (snd it)
           minY = Set.foldl (folder min) maxBound img
           maxY = Set.foldl (folder max) minBound img
 
-safeLookup :: Image -> (Int, Int) -> Int
-safeLookup image it = fromEnum $ Set.member it image
-
 runKernel :: Algo -> Image -> (Int, Int) -> Int
-runKernel algo img it = algo ! (toBinary subtiles)
+runKernel algo img it = algo ! toBinary subtiles
     where tiles = map kernelWindow (kernelWindow it)
+          safeLookup image it = fromEnum $ Set.member it image
           subtiles = map ((algo !) . toBinary . map (safeLookup img)) tiles
           toBinary = foldl (\ac it -> ac * 2 + it) 0
+          kernelWindow (i, j) = [(i + di, j + dj) | di <- [-1..1], dj <- [-1..1]]
 
 enhanceImage :: Algo -> Image -> Image
 enhanceImage algo img = (Set.fromList . map fst . filter (toEnum . snd)) applied
-    where ((minX, minY), (maxX, maxY)) = boundsImage img
-          flow = 6
+    where ((minX, minY), (maxX, maxY)) = bounds img
+          flow = 5
           grid = [(x, y) | x <- [minX - flow .. maxX + flow], y <- [minY - flow .. maxY + flow]]
           applied = zip grid $ map (runKernel algo img) grid
 
